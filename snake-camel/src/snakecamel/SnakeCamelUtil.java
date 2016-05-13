0a2af611e459bcade7f2157652f6ef34db141b18
g13943se <g13943se@m20202.tsuda.ac.jp>
2016-05-13 19:07:10 +0900

    テストケースは完成。ある程度修正(一部まだエラーあり要修正)

--- snake-camel/src/snakecamel/SnakeCamelUtil.java
+++ snake-camel/src/snakecamel/SnakeCamelUtil.java
@@ -5,27 +5,27 @@ public class SnakeCamelUtil {
 	public static String snakeToCamelcase(String snake_case) {
 		String[] words = snake_case.split("_");
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < words.length - 1; i++) {
+		for (int i = 0; i < words.length; i++) {
 			sb.append(capitalize(words[i]));
 		}
 		return new String(sb);
 	}
 	 
-	public static String camelToSnakecase(String camelcase) {
+	public static String camelToSnakecase(String camelCase) {
 		StringBuilder sb = new StringBuilder();
 		int j = 0;
-		for (int i = 0; i < camelcase.length(); i++) {
-			char c = camelcase.charAt(i);	// 今見てる文字が
+		for (int i = 0; i < camelCase.length(); i++) {
+			char c = camelCase.charAt(i);	// 今見てる文字が
 			if (Character.isUpperCase(c)) {	// 大文字だったら
-				sb.append(camelcase.substring(j, i));	// 前に大文字だったとこから今の地点までを+して
+				sb.append(camelCase.substring(j, i));	// 前に大文字だったとこから今の地点までを+して
 				if (sb.length() > 0) {	// 一番はじめの大文字でなければ、
 					sb.append("_");		// _を追加して
 				}
 				sb.append(Character.toLowerCase(c));	// 最後に感知した大文字を小文字になおして+する
-				j = i;	// jの位置訂正
+				j = i + 1;	// jの位置訂正
 			}
 		}
-		sb.append(camelcase.substring(j));
+		sb.append(camelCase.substring(j));
 		return new String(sb);
 	}
 	
@@ -42,5 +42,4 @@ public class SnakeCamelUtil {
 		String rest = s.substring(1);
 		return lowerFirst + rest;
 	}
-	
 }
--- snake-camel/test/snakecamel/SnakeCamelUtilTest.java
+++ snake-camel/test/snakecamel/SnakeCamelUtilTest.java
@@ -28,7 +28,7 @@ public class SnakeCamelUtilTest {
 		{ assertThat(SnakeCamelUtil.snakeToCamelcase("AbcDef"), is("Abcdef")); }
 	
 	@Test
-	public void スネークでもキャメルでもない文字列を渡した場合()
+	public void スネークでもキャメルでもない文字列を渡した場合_snake()
 		{ assertThat(SnakeCamelUtil.snakeToCamelcase("abcdef"), is("Abcdef")); }
 	
 	@Test
@@ -44,9 +44,28 @@ public class SnakeCamelUtilTest {
 	///////////////////////////////
 	
 	@Test
-	public void 通常のキャメルケースを渡した場合(){
-		
-	}
+	public void 通常のキャメルケースを渡した場合()
+		{ assertThat(SnakeCamelUtil.camelToSnakecase("AbcDefGhi"), is("abc_def_ghi")); }
+	
+	@Test
+	public void はじめが小文字の文字列を渡した場合()
+		{ assertThat(SnakeCamelUtil.camelToSnakecase("abcDefGhi"), is("abc_def_ghi")); }
+	
+	@Test
+	public void 最後が大文字の文字列を渡した場合()
+		{ assertThat(SnakeCamelUtil.camelToSnakecase("bcDefGhiA"), is("bc_def_ghi_a")); }
+	
+	@Test
+	public void スネークでもキャメルでもない文字列を渡した場合_camel()
+		{ assertThat(SnakeCamelUtil.camelToSnakecase("abcdef"), is("abcdef")); }
+	
+	@Test
+	public void 余分な空白がはじめに含まれている文字列を渡した場合()
+		{ assertThat(SnakeCamelUtil.camelToSnakecase(" AbcDef"), is("abc_def")); }
+	
+	@Test
+	public void 余分な空白が途中に含まれている文字列を渡した場合()
+		{ assertThat(SnakeCamelUtil.camelToSnakecase("Abc De f"), is("abc_def")); }
 
 }
 	
